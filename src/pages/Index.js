d37d4e8394f2ebd91dedb83ae8ebf0a8dcfce168
fxbk <44337498+fxbk@users.noreply.github.com>
2021-09-06 10:20:53 +0200

    Feat: Changed wording

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -16,7 +16,7 @@ const Index = () => (
             or you can check out my {' '}
             <Link to="/resume">resume</Link>, {' '}
             <Link to="/projects">projects</Link>, {' '}
-            view <Link to="/stats">site statistics</Link>, {' '}
+            view <Link to="/stats">some statistics about me</Link>, {' '}
             or <Link to="/contact">contact</Link> me.
           </p>
         </div>
