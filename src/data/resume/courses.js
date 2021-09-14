e7396650877b96b93f1acb82665a90648e8f12df
fxbk <44337498+fxbk@users.noreply.github.com>
2021-09-14 13:59:59 +0200

    Feat: Added more uni courses

--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -5,6 +5,48 @@ const courses = [
     link: 'https://niessner.github.io/I2DL/',
     university: 'TUM',
   },
+  {
+    title: 'Statistical Learning',
+    number: 'MA4802',
+    link: 'https://www.helmholtz-muenchen.de/icb/teaching/courses/stat-learning/index.html',
+    university: 'TUM',
+  },
+  {
+    title: 'TUM Data Innovation Lab',
+    number: 'MA8113',
+    link: 'https://www.di-lab.tum.de/tum-di-lab/',
+    university: 'TUM',
+  },
+  {
+    title: 'Foundations in Data Analysis',
+    number: 'MA4800',
+    link: '',
+    university: 'TUM',
+  },
+  {
+    title: 'Mathematical foundations of machine learning',
+    number: 'MA4801',
+    link: 'https://www-m5.ma.tum.de/Allgemeines/MA4801_2020S',
+    university: 'TUM',
+  },
+  {
+    title: 'Introduction: gAlgorithms and data structures',
+    number: 'IN0007',
+    link: 'https://campus.tum.de/tumonline/WBMODHB.wbShowMHBReadOnly?pKnotenNr=452818',
+    university: 'TUM',
+  },
+  {
+    title: 'Computational Statistics',
+    number: 'MA3402',
+    link: 'https://campus.tum.de/tumonline/wbLv.wbShowLVDetail?pStpSpNr=950400947&pSpracheNr=1',
+    university: 'TUM',
+  },
+  {
+    title: 'Nonlinear Optimization',
+    number: 'MA3503',
+    link: 'https://www-m1.ma.tum.de/bin/view/Lehrstuhl/MUlbrichNonlinearOptAdvWS1819',
+    university: 'TUM',
+  },
 ];
 
 export default courses;
