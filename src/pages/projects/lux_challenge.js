c301fc50b7d2d01a7119cbd596c3ef0852a66c2b
fxbk <44337498+fxbk@users.noreply.github.com>
2022-01-27 13:46:22 +0100

    Refactor: Added spaces in front of the links

--- src/pages/projects/lux_challenge.js
+++ src/pages/projects/lux_challenge.js
@@ -29,14 +29,14 @@ const lux_challenge = () => (
       <p>
         Our idea was to use a Graph Neural Network (GNN) and the <a href="https://arxiv.org/abs/1707.06347">Proximal Policy Optimization (PPO) algorithm</a>.
         The idea was based on the paper
-        <a href="https://arxiv.org/pdf/2107.08387.pdf">TRAIN ON SMALL, PLAY THE LARGE: SCALING UP BOARD GAMES WITH ALPHAZERO AND GNN</a>,
+        <a href="https://arxiv.org/pdf/2107.08387.pdf"> TRAIN ON SMALL, PLAY THE LARGE: SCALING UP BOARD GAMES WITH ALPHAZERO AND GNN</a>,
         where each node in the GNN represents one position on a board game. As the authors showed, the benefit of using GNN is the
         possibility of training the node neural network first on a small board game, learning first basic strategies, and then to
         scale up the board and let the agent learn more complex strategies.
       </p>
       <p>
         We implemented our approach, using PyTorch and RLlib. The repository can be found
-        <a href="https://github.com/cerebro-ai/lux-ai-2021">here</a>.
+        <a href="https://github.com/cerebro-ai/lux-ai-2021"> here</a>.
       </p>
       <p>
         Unfortunately, we fell into a really common RL fallacy.
